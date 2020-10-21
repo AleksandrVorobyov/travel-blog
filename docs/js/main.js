bf1a05d188a5547777d1de39b0c54a3ce92567da
Alex <sasha89253215539@yandex.ru>
2020-10-21 16:36:26 +0300

    Добавил бургер меню

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,12 +1,48 @@
 $(document).ready(function(){
-    $(".carousel__head").owlCarousel({
-      items: 1,
-      nav:false,
-      autoplay: true,
-      autoplayTimeout: 10000,
-      smartSpeed:1500,
-      loop: true,
-      margin: 10,
-      dots: true
-    });
+  $(".carousel__head").owlCarousel({
+    items: 1,
+    nav:false,
+    autoplay: true,
+    autoplayTimeout: 10000,
+    smartSpeed:1500,
+    loop: true,
+    margin: 10,
+    dots: true
   });
+});
+
+$(function () {
+  $('a[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1500
+        );
+        return false;
+      }
+    }
+  });
+});
+  
+$(document).ready(function() {
+  $('.nav__menu').click(function() {
+    $('.burger-menu').toggleClass("burger-menu--active");
+    $('body').toggleClass('lock');
+  });
+});
+
+$(document).ready(function() {
+  $('.link-burger').click(function() {
+    $('.burger-menu').removeClass("burger-menu--active");
+    $('body').removeClass('lock');
+  });
+});
